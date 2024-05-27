7c90df9344b773e7c3bfd27fa168cbf516ad7bea
Rohit Sandhu <rohit.sandhu@drishinfo.com>
2024-05-27 10:47:19 +0530

    Updated Code

---
+++ EmailValidator/EmailValidator.h
@@ -0,0 +1,18 @@
+//
+//  EmailValidator.h
+//  EmailValidator
+//
+//  Created by Rohit Sandhu on 27/05/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EmailValidator.
+FOUNDATION_EXPORT double EmailValidatorVersionNumber;
+
+//! Project version string for EmailValidator.
+FOUNDATION_EXPORT const unsigned char EmailValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmailValidator/PublicHeader.h>
+
+
